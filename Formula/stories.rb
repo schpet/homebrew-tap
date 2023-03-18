42e21f7ec125b9540e917a0330e9d224065c1679
Peter Schilling <peter@peterschilling.org>
2023-03-17 22:39:09 -0700

    chore: remove macos block (see if that fixes ci?)

--- Formula/stories.rb
+++ Formula/stories.rb
@@ -4,10 +4,8 @@ class Stories < Formula
   license "MIT"
   version "0.2.6"
 
-  on_macos do
-    url "https://github.com/schpet/stories/releases/download/v0.2.6/stories-x86_64-apple-darwin.tar.gz"
-    sha256 "011053f98d26c7f51ec6211c495898901d742d3b55c0f8ad92287982a8a36d1e"
-  end
+  url "https://github.com/schpet/stories/releases/download/v0.2.6/stories-x86_64-apple-darwin.tar.gz"
+  sha256 "011053f98d26c7f51ec6211c495898901d742d3b55c0f8ad92287982a8a36d1e"
 
   def install
     bin.install("stories")
