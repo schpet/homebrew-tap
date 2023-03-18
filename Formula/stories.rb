938b325d60eb1bff8ead1544422c3fc2d60bf831
Peter Schilling <peter@peterschilling.org>
2023-03-17 21:35:59 -0700

    Add a version, move binary to mac block

--- Formula/stories.rb
+++ Formula/stories.rb
@@ -1,11 +1,15 @@
 class Stories < Formula
   desc "CLI for pivotal tracker"
   homepage "https://github.com/schpet/stories"
-  url "https://github.com/schpet/stories/releases/download/v0.2.4/stories-x86_64-apple-darwin.tar.gz"
-  sha256 "477ea3663210822357c2554390b6b8ee775079d09178b36858113dc3ac84b71f"
   license "MIT"
+  version "0.2.4"
+
+  on_macos do
+    url "https://github.com/schpet/stories/releases/download/v0.2.4/stories-x86_64-apple-darwin.tar.gz"
+    sha256 "477ea3663210822357c2554390b6b8ee775079d09178b36858113dc3ac84b71f"
+  end
 
   def install
-    bin.install "stories"
+    bin.install("stories")
   end
 end
