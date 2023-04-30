3425cd65f84316196e1a50cce8be754f7a6c0a60
Peter Schilling <peter@peterschilling.org>
2023-04-30 08:18:42 -0700

    stories 0.3.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/stories.rb
+++ Formula/stories.rb
@@ -2,10 +2,10 @@ class Stories < Formula
   desc "CLI for pivotal tracker"
   homepage "https://github.com/schpet/stories"
   license "MIT"
-  version "0.3.1"
+  version "0.3.2"
 
-  url "https://github.com/schpet/stories/releases/download/v0.3.1/stories-x86_64-apple-darwin.tar.gz"
-  sha256 "037a4cbb9375a62b9359625d16147964e13220f2940bb1d53da12ff1bd005e17"
+  url "https://github.com/schpet/stories/releases/download/v0.3.2/stories-x86_64-apple-darwin.tar.gz"
+  sha256 "92b6b019ed7e7df66a8e51cd6e165c70e7194f9f99d83328ec0eb45de7d8b111"
 
   def install
     bin.install("stories")
