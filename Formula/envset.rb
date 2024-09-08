876a761de8f321623b1b24ff17ad683878aec33f
Peter Schilling <git@schpet.com>
2024-09-07 20:45:44 -0700

    feat: update envset formula to use latest version

--- Formula/envset.rb
+++ Formula/envset.rb
@@ -1,7 +1,7 @@
 class Envset < Formula
   desc "A tool to set environment variables from .env files"
   homepage "https://github.com/schpet/envset"
-  url "https://github.com/schpet/envset.git", tag: "v0.1.0"
+  url "https://github.com/schpet/envset.git"
   license "MIT"
 
   depends_on "rust" => :build
